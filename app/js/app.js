536cffec8bc0cc5d0510d98435ebde4deaf9c3d3
MikhailBobrov <mikhailurban13@yandex.ru>
2020-10-14 10:57:17 +0500

    tabs done

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,31 @@
-$(document).ready(function () {
-    // $('body').hide()
+const buttons = document.querySelectorAll('.content__con');
+const tabItems = document.querySelectorAll('.tabs__item');
+
+buttons.forEach(elem => {
+    elem.addEventListener('click', function () {
+        let currentBtn = elem;
+        let tabId = elem.getAttribute('data-tab');
+        console.log(tabId);
+
+        let currentTab = document.querySelector(tabId);
+        console.log(currentTab);
+
+        if (!currentBtn.classList.contains('active')) {
+            buttons.forEach(elem=> {
+                elem.classList.remove('active');
+                elem.classList.remove('content__active');
+            })
+
+            tabItems.forEach(elem=> {
+                elem.classList.remove('active')
+            })
+
+            currentTab.classList.add('active');
+            currentBtn.classList.add('active');
+            currentBtn.classList.add('content__active');
+        }
+    })
+
 })
+
+document.querySelector('.content__con:nth-child(2)').click();
